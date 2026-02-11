3e4d916fd789d185b90c5e24335ac07e7006f74f
Muhammadhussein Ammari <xmha97@gmail.com>
2026-02-11 18:28:20 +0330

    Add beep sound

--- app/src/main/java/ir/ammari/MainActivity.java
+++ app/src/main/java/ir/ammari/MainActivity.java
@@ -1,15 +1,9 @@
 package ir.ammari.rasad;
 
 import android.app.Activity;
-import android.content.res.Configuration;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowInsets;
-import android.view.WindowInsetsController;
-import android.view.WindowManager;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.widget.TextView;
 
 import java.io.ByteArrayOutputStream;
@@ -24,10 +18,11 @@ public class MainActivity extends Activity {
         final TextView view = new TextView(this);
         view.setText("\n\n\nLoading");
         setContentView(view);
+        ToneGenerator toneGen = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
         new Thread(() -> {
             try {
-                final InputStream inputStream = (InputStream)
-                        new URL("https://raw.githubusercontent.com/xmha97/test/refs/heads/main/status").openConnection().getContent();
+                URL url = new URL("https://raw.githubusercontent.com/xmha97/test/refs/heads/main/status");
+                InputStream inputStream = url.openStream();
                 final int bufLen = 1024;
                 byte[] buf = new byte[bufLen];
                 ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -36,6 +31,7 @@ public class MainActivity extends Activity {
                     outputStream.write(buf, 0, readLen);
                 final String result = outputStream.toString();
                 runOnUiThread(() -> view.setText("\n\n\n" + result));
+                toneGen.startTone(ToneGenerator.TONE_PROP_BEEP, 200);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
